4be81b640efca53eab66b0f4d3a0dd278c796906
EASYMAK777 <jplocallawncare@gmail.com>
2020-08-14 17:58:08 -0400

    successfully addded an icon

--- Assets/script.js
+++ Assets/script.js
@@ -146,4 +146,10 @@ $.ajax({
     for (i = element3PMFirstAppears; i < 40; i+=8) {
         var sectionNbr = "#section" + i;
         var newSection = $("<section>",{class: "col-lg-2", id: sectionNbr});           
+        var newCard = $("<div>").addClass("card bg-primary text-white");            
+        var newDiv = $("<div>").addClass("card-body");
+        var newH5 = $("<h5>",{class: "card-title", text: moment(fiveDaysWeather.list[i].dt_txt).format('MM/DD/YYYY')});
+        
+        var icon =fiveDaysWeather.list[i].weather[0].icon;
+        var iconURL = "https://openweathermap.org/img/wn/" + icon + ".png"
         
