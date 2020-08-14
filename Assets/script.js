49b9b427a21a47a108c913e3fe7ae6492901345a
EASYMAK777 <jplocallawncare@gmail.com>
2020-08-13 21:39:04 -0400

    added a call for the get current weather function to get uv index

--- Assets/script.js
+++ Assets/script.js
@@ -67,4 +67,56 @@ function getCurrentWeather(thisCity, id) {
 //get 5 day forecast for the selected City
 function getForecast(thisCity, id) {
     var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=${thisCity}&units=imperial&appid=${id}";
+
+    //using ajax to pull forecast information from forecast URL
+    $.ajax({
+        url: forecastURL,
+        method:"GET"
+    }).then(function (data){
+        for (i =0; i <data.list.length; i++) {
+            if data.list[i].dt_txt.search("15:00:00") != -1) {
+                var forcastDate = data.list[i];
+                //append the forecast to the list
+                $("forecast").append(
+                    `<div class="card bg-primary shadow m-4">
+                    <div class="card-body">
+                        <h4 class="card-title">${(new Date(1000 * forecastDate.dt).getUTCMonth()) + 1}/${new Date(1000 * forecastDate.dt).getUTCDate()}/${new Date(1000 * forecastDate.dt).getUTCFullYear()}</h4>
+                        <div class="card-text">
+                            <img src="http://openweathermap.org/img/w/${forecastDate.weather[0].icon}.png">
+                            <p class="card-text">Temp: ${forecastDate.main.temp} &degF</p>
+                            <p class="card-text">Humidity: ${forecastDate.main.humidity} %</p>
+                        </div>
+                    </div>
+                </div>`
+                );
+            }
+        }
+    })
+}
+
+// called getCurrentWeather function to get uv index for the selected city
+
+function getCurrentWeather(thisCity, id) {
+    var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${thisCity}&units=imperial&appid=${id}`;
+    var cityLat;
+    var cityLong;
+
+    $.ajax({
+        url: weatherURL,
+        method: "GET"
+    }).then(function (data) {
+        $(".cityToday").append(
+            `<div class="row ml-1">
+                <h3 class="mr-3">${data.name} (${(new Date(1000 * data.dt).getUTCMonth()) + 1}/${(new Date(1000 * data.dt).getUTCDate()) - 1}/${new Date(1000 * data.dt).getUTCFullYear()})</h3>
+                <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
+            </div>`
+        )
+        $(".cityToday").append(`<p>Temperature: ${data.main.temp} &degF</p>`)
+        $(".cityToday").append(`<p>Humidity: ${data.main.humidity} %</p>`)
+        $(".cityToday").append(`<p>Wind: ${data.wind.speed} mph</p>`)
+        cityLat = data.coord.lat;
+        cityLong = data.coord.lon;
+        getUVI(id, cityLat, cityLong);
+    })
+
 }
